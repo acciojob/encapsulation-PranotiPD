647e458cc9de4e6038cf4a73151bf92ca572ca40
Pranotii <pranoti0820@gmail.com>
2022-10-11 18:18:32 +0530

    2nd commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args){
         RWOnly obj = new RWOnly();
-        obj.setter("Neela");
-        obj.getter();
+        obj.setName("Neela");
+        obj.getName();
     }
 }
